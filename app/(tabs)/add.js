2b68508b18d28018d993bbd7c836a044f9495617
gracenev <144965499+gracenev@users.noreply.github.com>
2024-11-30 15:44:56 -0500

    added expenses and scrolling

--- app/(tabs)/add.js
+++ app/(tabs)/add.js
@@ -1,24 +1,88 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import React, { useRef, useEffect } from "react";
 import Icon from "react-native-vector-icons/MaterialIcons"; // MaterialIcons is the font family for Material Icons
-import { ScrollView } from "react-native";
+import { useIsFocused } from "@react-navigation/native";
 
 const add = () => {
+  const scrollViewRef = useRef(null);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused) {
+      scrollViewRef.current?.scrollTo({ y: 0, animated: true });
+      console.log("Screen reset on focus");
+    }
+  }, [isFocused]);
+
   return (
     <View className="flex-1 mt-40 items-center">
       <Text className="text-4xl text-customBlue top-2 text-center w-[250px] pb-28">
         Select Category to Add Expense
       </Text>
 
-      <ScrollView horizontal={true}>
-        <View className="items-center">
+      <ScrollView horizontal={true} ref={scrollViewRef}>
+        <View className="items-center px-28">
           <Icon
             name="fastfood"
-            size={150}
+            size={200}
             color="navy"
-            className="icon-customBlue p-7"
+            className="icon-customBlue"
           />
-          <Text className="text-3xl text-customBlue">Food/Grocery</Text>
+          <Text className="text-3xl text-customBlue">Food</Text>
+        </View>
+        <View className="items-center px-20">
+          <Icon
+            name="local-taxi"
+            size={200}
+            color="navy"
+            className="icon-customBlue"
+          />
+          <Text className="text-3xl text-customBlue">Transport</Text>
+        </View>
+        <View className="items-center px-20">
+          <Icon
+            name="movie"
+            size={200}
+            color="navy"
+            className="icon-customBlue"
+          />
+          <Text className="text-3xl text-customBlue">Entertainment</Text>
+        </View>
+        <View className="items-center px-20">
+          <Icon
+            name="shopping-bag"
+            size={200}
+            color="navy"
+            className="icon-customBlue"
+          />
+          <Text className="text-3xl text-customBlue">Shopping</Text>
+        </View>
+        <View className="items-center px-20">
+          <Icon
+            name="health-and-safety"
+            size={200}
+            color="navy"
+            className="icon-customBlue"
+          />
+          <Text className="text-3xl text-customBlue">Health</Text>
+        </View>
+        <View className="items-center px-20">
+          <Icon
+            name="school"
+            size={200}
+            color="navy"
+            className="icon-customBlue"
+          />
+          <Text className="text-3xl text-customBlue">Education</Text>
+        </View>
+        <View className="items-center px-28">
+          <Icon
+            name="credit-card"
+            size={200}
+            color="navy"
+            className="icon-customBlue"
+          />
+          <Text className="text-3xl text-customBlue">Bill Payment</Text>
         </View>
       </ScrollView>
     </View>
