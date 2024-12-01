efc52da4321a3229a13ab092c1e23d0ac2bca4e7
gracenev <144965499+gracenev@users.noreply.github.com>
2024-11-30 21:20:46 -0500

    added input amount functionality

--- app/(tabs)/add.js
+++ app/(tabs)/add.js
@@ -1,11 +1,38 @@
-import { ScrollView, Text, View } from "react-native";
-import React, { useRef, useEffect } from "react";
-import Icon from "react-native-vector-icons/MaterialIcons"; // MaterialIcons is the font family for Material Icons
+import {
+  ScrollView,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useRef, useEffect, useState } from "react";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import { useIsFocused } from "@react-navigation/native";
 
 const add = () => {
   const scrollViewRef = useRef(null);
   const isFocused = useIsFocused();
+  const [showInput, setShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [submitStatus, setSubmitStatus] = useState(false);
+  const [showText, setShowText] = useState(false);
+
+  const handlePress = (category) => {
+    setSelectedCategory(category);
+    console.log("category: ", category);
+    setShowInput(true);
+  };
+
+  const handleSubmit = () => {
+    setShowText(true);
+    console.log(inputValue);
+    setTimeout(() => {
+      setShowText(false);
+      setShowInput(false);
+      setInputValue("");
+    }, 2500);
+  };
 
   useEffect(() => {
     if (isFocused) {
@@ -20,71 +47,109 @@ const add = () => {
         Select Category to Add Expense
       </Text>
 
-      <ScrollView horizontal={true} ref={scrollViewRef}>
-        <View className="items-center px-28">
-          <Icon
-            name="fastfood"
-            size={200}
-            color="navy"
-            className="icon-customBlue"
-          />
-          <Text className="text-3xl text-customBlue">Food</Text>
+      <ScrollView horizontal={true} ref={scrollViewRef} className="h-48">
+        <View className="px-28">
+          <TouchableOpacity className="items-center">
+            <Icon
+              name="fastfood"
+              size={200}
+              color="navy"
+              className="icon-customBlue"
+              onPress={() => handlePress("Food")}
+            />
+            <Text className="text-3xl text-customBlue">Food</Text>
+          </TouchableOpacity>
         </View>
-        <View className="items-center px-20">
-          <Icon
-            name="local-taxi"
-            size={200}
-            color="navy"
-            className="icon-customBlue"
-          />
-          <Text className="text-3xl text-customBlue">Transport</Text>
+        <View className="px-20">
+          <TouchableOpacity className="items-center">
+            <Icon
+              name="local-taxi"
+              size={200}
+              color="navy"
+              className="icon-customBlue"
+              onPress={() => handlePress("Transport")}
+            />
+            <Text className="text-3xl text-customBlue">Transport</Text>
+          </TouchableOpacity>
         </View>
-        <View className="items-center px-20">
-          <Icon
-            name="movie"
-            size={200}
-            color="navy"
-            className="icon-customBlue"
-          />
-          <Text className="text-3xl text-customBlue">Entertainment</Text>
+        <View className="px-20">
+          <TouchableOpacity className="items-center">
+            <Icon
+              name="movie"
+              size={200}
+              color="navy"
+              className="icon-customBlue"
+              onPress={() => handlePress("Entertainment")}
+            />
+            <Text className="text-3xl text-customBlue">Entertainment</Text>
+          </TouchableOpacity>
         </View>
-        <View className="items-center px-20">
-          <Icon
-            name="shopping-bag"
-            size={200}
-            color="navy"
-            className="icon-customBlue"
-          />
-          <Text className="text-3xl text-customBlue">Shopping</Text>
+        <View className="px-20">
+          <TouchableOpacity className="items-center">
+            <Icon
+              name="shopping-bag"
+              size={200}
+              color="navy"
+              className="icon-customBlue"
+              onPress={() => handlePress("Shopping")}
+            />
+            <Text className="text-3xl text-customBlue">Shopping</Text>
+          </TouchableOpacity>
         </View>
-        <View className="items-center px-20">
-          <Icon
-            name="health-and-safety"
-            size={200}
-            color="navy"
-            className="icon-customBlue"
-          />
-          <Text className="text-3xl text-customBlue">Health</Text>
+        <View className="px-20">
+          <TouchableOpacity className="items-center">
+            <Icon
+              name="health-and-safety"
+              size={200}
+              color="navy"
+              className="icon-customBlue"
+              onPress={() => handlePress("Health")}
+            />
+            <Text className="text-3xl text-customBlue">Health</Text>
+          </TouchableOpacity>
         </View>
-        <View className="items-center px-20">
-          <Icon
-            name="school"
-            size={200}
-            color="navy"
-            className="icon-customBlue"
-          />
-          <Text className="text-3xl text-customBlue">Education</Text>
+        <View className="px-20">
+          <TouchableOpacity className="items-center">
+            <Icon
+              name="school"
+              size={200}
+              color="navy"
+              className="icon-customBlue"
+              onPress={() => handlePress("Education")}
+            />
+            <Text className="text-3xl text-customBlue">Education</Text>
+          </TouchableOpacity>
         </View>
-        <View className="items-center px-28">
-          <Icon
-            name="credit-card"
-            size={200}
-            color="navy"
-            className="icon-customBlue"
-          />
-          <Text className="text-3xl text-customBlue">Bill Payment</Text>
+        <View className="px-28">
+          <TouchableOpacity className="items-center">
+            <Icon
+              name="credit-card"
+              size={200}
+              color="navy"
+              className="icon-customBlue"
+              onPress={() => handlePress("Bill Payment")}
+            />
+            <Text className="text-3xl text-customBlue">Bill Payment</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
+
+      {showInput && (
+        <View>
+          <View className="flex-row align-center bg-slate-300 ">
+            <Text className="py-3 pl-3 font-bold text-xl">$</Text>
+            <TextInput
+              placeholder="Input Amount"
+              value={inputValue}
+              onChangeText={(text) => setInputValue(text)}
+            />
+          </View>
+          <TouchableOpacity onPress={handleSubmit} className="items-center p-7">
+            <Text className="bg-customPurple p-2">Submit</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {showText && <Text>{selectedCategory} Expense Submitted</Text>}
     </View>
   );
 };
